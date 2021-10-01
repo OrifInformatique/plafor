3ed9ef216713ea84138f0a808074906e65f8f764
Didier Viret <didier.viret@orif.ch>
2021-10-01 14:51:35 +0200

    Added index method in migration controller

--- orif/plafor/Controllers/Migration.php
+++ orif/plafor/Controllers/Migration.php
@@ -6,41 +6,44 @@ namespace Plafor\Controllers;
 
 use CodeIgniter\Config\Services;
 
-class Migration extends \CodeIgniter\Controller
-{
+class Migration extends \App\Controllers\BaseController {
+
     public function init()
     {
         if ($this->request->getPost('password')===$migrationPassword){
 
-        $file = fopen(WRITEPATH . 'appStatus.json', 'r+');
-        $initDatas = fread($file, 100);
+            $file = fopen(WRITEPATH . 'appStatus.json', 'r+');
+            $initDatas = fread($file, 100);
 
-        if ((json_decode($initDatas, true))['initialized'] === false) {
-            $this->response->setStatusCode('201')->send();
-            $migrate = Services::migrations();
-            try {
-                $migrate->setNamespace('User');
-                $migrate->latest();
-                $migrate->setNamespace('Plafor');
-                $migrate->latest();
-            } catch (\Exception $e) {
-                echo $e->getMessage();
+            if ((json_decode($initDatas, true))['initialized'] === false) {
+                $this->response->setStatusCode('201')->send();
+                $migrate = Services::migrations();
+                try {
+                    $migrate->setNamespace('User');
+                    $migrate->latest();
+                    $migrate->setNamespace('Plafor');
+                    $migrate->latest();
+                } catch (\Exception $e) {
+                    echo $e->getMessage();
+                }
+                fclose($file);
+                fwrite(fopen(WRITEPATH . 'appStatus.json', 'w+'), json_encode(['initialized' => true]));
+                unlink((new \ReflectionClass('\Plafor\Controllers\Migration'))->getFileName());
+                unlink(ROOTPATH.'orif/plafor/Views/migrationindex.php');
+                return $this->response->setStatusCode(200);
+
             }
-            fclose($file);
-            fwrite(fopen(WRITEPATH . 'appStatus.json', 'w+'), json_encode(['initialized' => true]));
-            unlink((new \ReflectionClass('\Plafor\Controllers\Migration'))->getFileName());
-            unlink(ROOTPATH.'orif/plafor/Views/migrationindex.php');
-            return $this->response->setStatusCode(200);
+            else{
+                return $this->response->setStatusCode('400');
+            }
+        }else{
+                return $this->response->setStatusCode('401');
+        }
+    }
 
-        }
-        else{
-            return $this->response->setStatusCode('400');
-        }
-    }else{
-            return $this->response->setStatusCode('401');
-        }
-
-}
+    public function index() {
+        $this->display_view("\Plafor\migrationIndex.php");
+    }
 }
 
 
