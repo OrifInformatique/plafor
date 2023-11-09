db33d2e510bb0b985a56809fd3f7647b826f5d25
Didier Viret <didier.viret@orif.ch>
2023-11-09 15:50:21 +0100

    Update old course plans official names

--- orif/plafor/Database/Seeds/addCoursePlanDatas.php
+++ orif/plafor/Database/Seeds/addCoursePlanDatas.php
@@ -12,10 +12,10 @@ class addCoursePlanDatas extends Seeder
     {
         //course_plan//
         $course_plan = array(
-            array('id' => '1','formation_number' => '88601','official_name' => ' Informaticien/-ne CFC Développement d\'applications','date_begin' => '2014-08-01','archive' => NULL),
-            array('id' => '2','formation_number' => '88602','official_name' => ' Informaticien/-ne CFC Informatique d\'entreprise','date_begin' => '2014-08-01','archive' => NULL),
-            array('id' => '3','formation_number' => '88603','official_name' => ' Informaticien/-ne CFC Technique des systèmes','date_begin' => '2014-08-01','archive' => NULL),
-            array('id' => '4','formation_number' => '88605','official_name' => ' Opératrice en informatique / Opérateur en informatique CFC','date_begin' => '2018-08-01','archive' => NULL)
+            array('id' => '1','formation_number' => '88601','official_name' => '[2014-2020] Informaticienne, Informaticien avec CFC, orientation développement d\'applications','date_begin' => '2014-08-01','archive' => NULL),
+            array('id' => '2','formation_number' => '88602','official_name' => '[2014-2020] Informaticienne, Informaticien avec CFC, orientation informatique d\'entreprise','date_begin' => '2014-08-01','archive' => NULL),
+            array('id' => '3','formation_number' => '88603','official_name' => '[2014-2020] Informaticienne, Informaticien avec CFC, orientation technique des systèmes','date_begin' => '2014-08-01','archive' => NULL),
+            array('id' => '4','formation_number' => '88605','official_name' => 'Opératrice en informatique / Opérateur en informatique avec CFC','date_begin' => '2018-08-01','archive' => NULL)
         );
 
         foreach ($course_plan as $course_plan){
