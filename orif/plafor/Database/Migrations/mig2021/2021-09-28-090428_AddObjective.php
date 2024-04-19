708ddd6a9338a20c33ed669196b1e922b681df2e
Didier Perroud <didier.perroud@formation.orif.ch>
2024-04-19 11:27:20 +0200

    Fix error in migrations 2021

--- orif/plafor/Database/Migrations/mig2021/2021-09-28-090428_AddObjective.php
+++ orif/plafor/Database/Migrations/mig2021/2021-09-28-090428_AddObjective.php
@@ -14,46 +14,12 @@ class AddObjective extends Migration {
 
     public function up() {
 
-        $this->db->disableForeignKeyChecks();
-        $this->forge->addField([
-            'id' => [
-                'type' => 'int',
-                'constraint' => '11',
-                'unsigned' => true,
-                'auto_increment' => true,
-            ],
-            'fk_operational_competence' => [
-                'type' => 'int',
-                'null'=>true,
-                'unsigned' => true,
-            ],
-            'symbol' => [
-                'type' => 'varchar',
-                'constraint' => '10',
-            ],
-            'taxonomy' => [
-                'type' => 'int', 
-                'constraint' => '5',
-            ],
-            'name' => [
-                'type' => 'varchar',
-                'constraint' => '350',
-            ],
-            'archive timestamp null',
-        ]);
-
-        $this->forge->addKey('id', true, true);
-        $this->forge->addForeignKey('fk_operational_competence','operational_competence','id');
-        try {
-            $this->db->tableExists('objective')===true?:$this->forge->createTable('objective');
-        }catch (\Exception $e){
-            echo $e->getMessage();
-        }
+        /* Add new datas for new "ordonnance sur la formation professionnelle" */
         $seeder = \Config\Database::seeder();
         $seeder->call('\Plafor\Database\Seeds\addObjective2021Datas');
     }
 
     public function down() {
-        $this->forge->dropTable('objective');
+        
     }
 }
