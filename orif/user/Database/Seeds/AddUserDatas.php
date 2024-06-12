714d835ce7f74d3d05d6827338e5824eb6e9f76e
Didier Viret <didier.viret@orif.ch>
2024-06-12 10:30:10 +0200

    Modify user seeders to match unit tests

--- orif/user/Database/Seeds/AddUserDatas.php
+++ orif/user/Database/Seeds/AddUserDatas.php
@@ -9,7 +9,7 @@ class AddUserDatas extends \CodeIgniter\Database\Seeder
     public function run()
     {
         $data = [
-            ['fk_user_type'=>1,'username'=>'admin','password'=>'$2y$10$DeYetQ9iAzLhKWU.BUesPOKSbsK4CExcDLYeqSmrGiR7O.EDjMlW2','email'=>'admin@admin.com'],
+            ['fk_user_type'=>1,'username'=>'admin','password'=>'$2y$10$DeYetQ9iAzLhKWU.BUesPOKSbsK4CExcDLYeqSmrGiR7O.EDjMlW2'],
             ['fk_user_type'=>2,'username'=>'trainer','password'=>'$2y$10$a2VV.wOG76z0Vw83U8ucs.XUYTV0Im5c1Z8Pgiq1OrDvh/2qFy9vq','email'=>'trainer@trainer.com'],
         ];
         foreach($data as $row) $this->db->table('user')->insert($row);
