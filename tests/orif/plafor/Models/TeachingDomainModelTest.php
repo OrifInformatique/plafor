ea0a5dbf2084ef19bc9bcbd7094e570084193f62
Frankva <24254885+Frankva@users.noreply.github.com>
2024-08-30 13:23:32 +0200

    tests: better findAll test for teaching domain model

--- tests/orif/plafor/Models/TeachingDomainModelTest.php
+++ tests/orif/plafor/Models/TeachingDomainModelTest.php
@@ -63,6 +63,8 @@ class TeachingDomainModelTest extends CIUnitTestCase
         $teachingDomainModel = model('TeachingDomainModel');
         $data = $teachingDomainModel->findAll();
         $this->assertIsArray($data);
+        $this->assertEquals(1, $data[0]['id']);
+        $this->assertEquals(2, $data[1]['id']);
     }
 
     /**
