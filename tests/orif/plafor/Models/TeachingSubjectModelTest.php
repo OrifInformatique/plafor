8f9e3245bd3a37da4745a07d44cd49a0e3fa0962
Frankva <24254885+Frankva@users.noreply.github.com>
2024-08-30 13:26:59 +0200

    tests: better findAll test for subject model

--- tests/orif/plafor/Models/TeachingSubjectModelTest.php
+++ tests/orif/plafor/Models/TeachingSubjectModelTest.php
@@ -69,6 +69,8 @@ class TeachingSubjectModelTest extends CIUnitTestCase
         $teachingSubjectModel = model('TeachingSubjectModel');
         $data = $teachingSubjectModel->findAll();
         $this->assertIsArray($data);
+        $this->assertEquals(1, $data[0]['id']);
+        $this->assertEquals(2, $data[1]['id']);
     }
 
     /**
