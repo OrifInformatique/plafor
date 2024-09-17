0016723f148fd36bb68d0b2464e23fce49a487fd
Jocelin Thumelin <jocelin.thumelin@formation.orif.ch>
2024-09-17 10:03:43 +0200

    Merge view resolve

--- orif/plafor/Controllers/CoursePlan.php
+++ orif/plafor/Controllers/CoursePlan.php
@@ -477,9 +477,7 @@ class CoursePlan extends \App\Controllers\BaseController
             }
         }
 
-        // Access permissions
-        if ($_SESSION['user_access'] >= config('\User\Config\UserConfig')->access_lvl_admin
-            || (isset($doesTrainerTrainsApprentice) && $doesTrainerTrainsApprentice))
+        if ($_SESSION['user_access'] < config('\User\Config\UserConfig')->access_lvl_admin && !$doesTrainerTrainsApprentice)
         {
             return $this->display_view('\User\errors\403error');
         }
@@ -523,20 +521,10 @@ class CoursePlan extends \App\Controllers\BaseController
 
                 $this->acquisition_status_model->where('fk_user_course', $user_course_id)->delete();
 
-                    // Deletes user's course
-                    $this->user_course_model->delete($user_course_id);
-
-                    return redirect()->to(base_url('plafor/apprentice/list_user_courses/'.$apprentice['id']));
-
-                default:
-                    // Do nothing
-            }
-
-            return redirect()->to(base_url('plafor/apprentice/list_apprentice'));
+                $this->user_course_model->delete($user_course_id);
         }
 
-        else
-            return $this->display_view('\User\errors\403error');
+        return redirect()->to(base_url('plafor/apprentice/list_user_courses/'.$apprentice['id']));
     }
 
     /**
@@ -772,14 +760,14 @@ class CoursePlan extends \App\Controllers\BaseController
         $course_plan['date_begin'] = $date_begin->toLocalizedString('dd.MM.Y');
 
         $teaching_domains = [];
-
+        
         // Get teaching domains, subjects and modules
         foreach ($this->m_teaching_domain_model->where("fk_course_plan", $course_plan_id)->findAll() as $domain) {
             $teaching_subject = [];
             
             // Get teaching subjects of the domain
             foreach ($this->m_teaching_subject_model->where("fk_teaching_domain", $domain["id"])->findAll() as $subject){
-                $teaching_subject[] = [
+                $teaching_subject[] = [ 
                     "id"            => $subject["id"],               // ID of the subject. Required.
                     "name"          => $subject["name"],             // Name of the subject. Required.
                     "weighting"     => $subject["subject_weight"],   // Weighing of the subject (in the domain average). Required.
