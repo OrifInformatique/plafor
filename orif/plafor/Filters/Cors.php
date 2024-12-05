abfba3e074f6edaba90a140c8c634059e29077de
Dervey Dylan <114241438+derveydylan@users.noreply.github.com>
2024-12-05 16:40:19 +0100

    Update Cors.php

--- orif/plafor/Filters/Cors.php
+++ orif/plafor/Filters/Cors.php
@@ -10,18 +10,16 @@ class Cors implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        // Gérer les en-têtes CORS
-        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header("Access-Control-Allow-Origin: http://localhost:4000");
         header("Access-Control-Allow-Methods: GET");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-        // Si la méthode est OPTIONS, répondre directement avec un statut 200
         if ($request->getMethod() === 'options')
             return service('response')->setStatusCode(200);
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        // Pas besoin de gérer quoi que ce soit ici pour CORS
+
     }
 }
